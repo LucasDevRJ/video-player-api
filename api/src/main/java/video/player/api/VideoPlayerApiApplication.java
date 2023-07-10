ae940b70c01c25d41b71d56d3da71a01bc831b7e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-10 20:02:02 -0300

    adição do projeto

---
+++ api/src/main/java/video/player/api/VideoPlayerApiApplication.java
@@ -0,0 +1,13 @@
+package video.player.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoPlayerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoPlayerApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/video/player/api/VideoPlayerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package video.player.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoPlayerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
