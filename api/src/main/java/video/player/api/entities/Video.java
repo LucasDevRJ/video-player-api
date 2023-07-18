8b0ed5c2a8a8d2dcd1093f5ad0101d293dfe17a3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-18 20:40:59 -0300

    creating attributes and annotations

--- api/src/main/java/video/player/api/entities/Video.java
+++ api/src/main/java/video/player/api/entities/Video.java
@@ -1,6 +1,26 @@
 package video.player.api.entities;
 
+import jakarta.persistence.*;
+
+import java.io.Serial;
+
+@Entity
+@Table(name = "tb_video")
 public class Video {
+    @Serial
+    private static final Integer serialVersionUID = 1;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String email;
     private String senha;
+    @ManyToOne
+    private Channel channel;
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
 }
