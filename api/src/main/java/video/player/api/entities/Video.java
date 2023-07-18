ca08a73ad42ece90803aa818a53c82cdb0553703
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-18 20:42:56 -0300

    generating constructor

--- api/src/main/java/video/player/api/entities/Video.java
+++ api/src/main/java/video/player/api/entities/Video.java
@@ -3,10 +3,11 @@ package video.player.api.entities;
 import jakarta.persistence.*;
 
 import java.io.Serial;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "tb_video")
-public class Video {
+public class Video implements Serializable {
     @Serial
     private static final Integer serialVersionUID = 1;
     @Id
@@ -16,6 +17,11 @@ public class Video {
     private String senha;
     @ManyToOne
     private Channel channel;
+    public Video(String email, String senha, Channel channel) {
+        this.email = email;
+        this.senha = senha;
+        this.channel = channel;
+    }
     public String getEmail() {
         return email;
     }
