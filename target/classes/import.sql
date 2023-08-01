6450ab2d9d0e321f607def62b030e3903df2c526
Giana Medeiros <gianamedeiros.00510@gmail.com>
2023-07-31 22:27:51 -0300

    created update method and seeding tb_channel

---
+++ target/classes/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_account(name, email, password) VALUES('Gabriel Henrique' , 'gabriel@gmail.com', '321654987');
+INSERT INTO tb_account(name, email, password) VALUES('Jorge Cunha' , 'jorge@gmail.com', '456123789');
+INSERT INTO tb_account(name, email, password) VALUES('Larissa Dias' , 'larissa@gmail.com', '852167943');
+INSERT INTO tb_account(name, email, password) VALUES('Fernanda Lins' , 'fernanda@gmail.com', '147258369');
+INSERT INTO tb_account(name, email, password) VALUES('Jessica Cordeiro' , 'jessica@gmail.com', '963852741');
+INSERT INTO tb_account(name, email, password) VALUES('Wellington Silva' , 'wellington@gmail.com', '1425367854');
+
+INSERT INTO tb_channel(name, description, url) VALUES('Façanhas do Bear','Jogo Zelando a colmeia', ' https://www.youtube.com/watch?v=So2zruUYpgU');
+INSERT INTO tb_channel(name, description, url) VALUES('Orvalho','Divulgação de mensagens', ' https://www.youtube.com/@OrvalhoPontoCom');
+INSERT INTO tb_channel(name, description, url) VALUES('Luluca','Ensinando como jogar em diversas plataformas', ' ');
+INSERT INTO tb_channel(name, description, url) VALUES('Mussoumano','Garten of Banban2', 'https://www.youtube.com/watch?v=cwNzlJhh0R4 ');
