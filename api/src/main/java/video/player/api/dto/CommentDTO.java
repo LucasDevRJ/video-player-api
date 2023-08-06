082b6b4d2b96669bbbd4672efc669c470e9b1bc6
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-06 16:06:43 -0300

    creation of getters and setters

--- api/src/main/java/video/player/api/dto/CommentDTO.java
+++ api/src/main/java/video/player/api/dto/CommentDTO.java
@@ -10,4 +10,52 @@ public class CommentDTO {
     private Date upload;
     private int numeroVisualizacoes;
     //private List<Comments> comentarios;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getUrlVideo() {
+        return urlVideo;
+    }
+
+    public void setUrlVideo(String urlVideo) {
+        this.urlVideo = urlVideo;
+    }
+
+    public Date getUpload() {
+        return upload;
+    }
+
+    public void setUpload(Date upload) {
+        this.upload = upload;
+    }
+
+    public int getNumeroVisualizacoes() {
+        return numeroVisualizacoes;
+    }
+
+    public void setNumeroVisualizacoes(int numeroVisualizacoes) {
+        this.numeroVisualizacoes = numeroVisualizacoes;
+    }
 }
