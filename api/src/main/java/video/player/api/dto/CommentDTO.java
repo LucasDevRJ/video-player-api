f56ad5377fb613d393c2f09fddb5d57084733fe4
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-06 16:09:08 -0300

    generating constructors

--- api/src/main/java/video/player/api/dto/CommentDTO.java
+++ api/src/main/java/video/player/api/dto/CommentDTO.java
@@ -1,8 +1,12 @@
 package video.player.api.dto;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Date;
 
-public class CommentDTO {
+public class CommentDTO implements Serializable {
+    @Serial
+    private static final Integer serialVersionUID= 1;
     private String titulo;
     private String descricao;
     private int duracao;
@@ -10,6 +14,19 @@ public class CommentDTO {
     private Date upload;
     private int numeroVisualizacoes;
     //private List<Comments> comentarios;
+
+    public CommentDTO(String titulo, String descricao, int duracao, String urlVideo, Date upload, int numeroVisualizacoes) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.duracao = duracao;
+        this.urlVideo = urlVideo;
+        this.upload = upload;
+        this.numeroVisualizacoes = numeroVisualizacoes;
+    }
+
+    public CommentDTO() {
+
+    }
 
     public String getTitulo() {
         return titulo;
