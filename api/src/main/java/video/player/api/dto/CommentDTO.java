748dea5d718b25bc7e8f1baa729148e4e491e5fe
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-06 16:05:17 -0300

    creation of the Comments Class and its attributes

---
+++ api/src/main/java/video/player/api/dto/CommentDTO.java
@@ -0,0 +1,13 @@
+package video.player.api.dto;
+
+import java.util.Date;
+
+public class CommentDTO {
+    private String titulo;
+    private String descricao;
+    private int duracao;
+    private String urlVideo;
+    private Date upload;
+    private int numeroVisualizacoes;
+    //private List<Comments> comentarios;
+}
